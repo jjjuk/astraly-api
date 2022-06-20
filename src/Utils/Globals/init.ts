72cb27a3600a458db4e3d32f2ddc384d809ba175
Evolve <Artevolve@yahoo.com>
2022-06-20 21:45:26 +0200

    fix: env variables ✍️
    - change to PORT for heroku

--- src/Utils/Globals/init.ts
+++ src/Utils/Globals/init.ts
@@ -2,8 +2,8 @@ import { globals } from './index'
 
 export const initGlobals = (): void => {
   globals.APP_URL = process.env.APP_URL
-  globals.PORT = process.env.SERVER_PORT
-  globals.HOST = process.env.SERVER_HOST
+  globals.PORT = process.env.PORT
+  globals.HOST = process.env.HOST
   globals.DB_HOST = process.env.DB_HOST
   globals.DB_NAME = process.env.DB_NAME
   globals.API_URL = process.env.API_URL
