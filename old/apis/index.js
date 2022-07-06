c6d812c67e7230643175b24d76ea039bf18670c7
Evolve <Artevolve@yahoo.com>
2022-07-06 19:47:34 +0200

    fix: remove old API 🔨

--- old/apis/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-router.use("/auth", require("./auth"));
-router.use("/account", require("./account"));
-router.use("/contact", require("./contact"));
-router.use("/quest", require("./quest"));
-
-module.exports = router;
