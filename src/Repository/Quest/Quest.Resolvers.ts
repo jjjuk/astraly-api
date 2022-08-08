48ff37ae11a011ba271ebb7b243fc26044b79e27
Evolve <Artevolve@yahoo.com>
2022-08-08 17:33:39 +0200

    Update Quest.Resolvers.ts

--- src/Repository/Quest/Quest.Resolvers.ts
+++ src/Repository/Quest/Quest.Resolvers.ts
@@ -5,10 +5,12 @@ import { AccountModel } from '../Account/Account.Entity'
 import { QuestHistoryModel } from './QuestHistory.Entity'
 import { MerkleProofsModel } from './MerkleProofs.Entity'
 // import { UserAccess } from '../../Modules/Auth/AuthChecker'
-import { validateAndParseAddress } from 'starknet'
+// import { validateAndParseAddress } from 'starknet'
 import { DocumentType } from '@typegoose/typegoose'
 import { UserAccess } from '../../Modules/Auth/AuthChecker'
 import { QuestInput } from './Quest.InputTypes'
+import { BigNumber } from 'ethers'
+import { hexValue } from 'ethers/lib/utils'
 
 @Resolver()
 export class QuestResolvers {
@@ -61,7 +63,9 @@ export class QuestResolvers {
     const merkleProofs = await MerkleProofsModel.findOne({
       idoId: Number(idoId),
     }).exec()
-    const proof = merkleProofs.data[address]
+    const trimmed = hexValue(BigNumber.from(address).toHexString())
+    console.log(merkleProofs.data['0x2356b628d108863baf8644c945d97bad70190af5957031f4852d00d0f690a77'], trimmed)
+    const proof = merkleProofs.data[trimmed]
 
     if (!proof) {
       throw new Error('no proof found for address')
@@ -71,15 +75,15 @@ export class QuestResolvers {
   }
 
   @Query(() => Quest)
-  async quest (@Arg('_id') _id: string): Promise<DocumentType<Quest>> {
+  async quest(@Arg('_id') _id: string): Promise<DocumentType<Quest>> {
     return await QuestModel.findById(_id).exec()
   }
 
   @Query(() => [Quest])
-  async quests (@Arg('idoId', { nullable: true }) idoId?: string): Promise<Array<DocumentType<Quest>>> {
+  async quests(@Arg('idoId', { nullable: true }) idoId?: string): Promise<Array<DocumentType<Quest>>> {
     if (idoId) {
       return await QuestModel.find({
-        idoId
+        idoId,
       }).exec()
     }
 
@@ -88,18 +92,18 @@ export class QuestResolvers {
 
   @Authorized([UserAccess.Admin])
   @Mutation(() => Quest)
-  async updateQuest (@Arg('data') data: QuestInput): Promise<DocumentType<Quest>> {
+  async updateQuest(@Arg('data') data: QuestInput): Promise<DocumentType<Quest>> {
     const { _id, ...d } = data
     if (_id) {
       return await QuestModel.findByIdAndUpdate(_id, {
         $set: {
           ...d,
-        }
+        },
       })
     }
 
     return await QuestModel.create({
-      ...d
+      ...d,
     })
   }
 }
